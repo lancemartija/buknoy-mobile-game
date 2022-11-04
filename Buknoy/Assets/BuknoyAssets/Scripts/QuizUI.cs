917c14badba136aebd6a39489a32d5e17e54e7ca
CarlIvan613 <95407957+CarlIvan613@users.noreply.github.com>
2022-11-04 16:43:04 +0800

    moved button functions here to reduce clutter

--- Buknoy/Assets/BuknoyAssets/Scripts/QuizUI.cs
+++ Buknoy/Assets/BuknoyAssets/Scripts/QuizUI.cs
@@ -9,7 +9,7 @@ public class QuizUI : MonoBehaviour
     [SerializeField] public QuizManager quizmanager;
 
     
-
+    //Quiz Hub Buttons
     public void Button1Click()
     {
         quizmanager.StartGame(1);
@@ -33,5 +33,28 @@ public class QuizUI : MonoBehaviour
     public void BacktoMenuHub()
     {
         SceneManager.LoadScene("MenuHub");
+    }
+    //Quiz Game Buttons
+    public void PauseButton()
+    {
+
+       quizmanager.gamestatus = GameStatus.Menu;
+       quizmanager.PausePanel.SetActive(true);
+    }
+    public void ConfirmExitButton()
+    {
+        quizmanager.ConfirmExitPanel.SetActive(true);
+    }
+    public void ResumeButton()
+    {
+
+        quizmanager.gamestatus = GameStatus.Playing;
+        quizmanager.PausePanel.SetActive(false);
+        quizmanager.ConfirmExitPanel.SetActive(false);
+    }
+
+    public void RetryButton ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
