f1a57c162119094b68cd4e433c2d5881d10c20cf
RonJMA5 <97772668+RonJMA5@users.noreply.github.com>
2022-12-18 21:03:45 +0800

    Improved A* script

--- Buknoy/Assets/EnemyAI.cs
+++ Buknoy/Assets/EnemyAI.cs
@@ -5,30 +5,23 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
-    [Header("Pathfinding")]
     public Transform target;
     public float activateDistance = 50f;
     public float pathUpdateSeconds = 0.5f;
 
-    [Header("Physics")]
     public float speed = 200f;
     public float nextWaypointDistance = 3f;
-    public float jumpNodeHeightRequirement = 0.8f;
-    public float jumpModifier = 0.3f;
-    public float jumpCheckOffset = 0.1f;
 
-    [Header("Custom Behavior")]
-    public bool followEnabled = true;
-    public bool jumpEnabled = true;
-    public bool directionLookEnabled = true;
+    public Transform enemyGFX;
 
-    private Path path;
-    private int currentWaypoint = 0;
-    RaycastHit2D isGrounded;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
     Seeker seeker;
     Rigidbody2D rb;
 
-    public void Start()
+    void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
@@ -36,74 +29,10 @@ public class EnemyAI : MonoBehaviour
         InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
     }
 
-    private void FixedUpdate()
+    void UpdatePath()
     {
-        if (TargetInDistance() && followEnabled)
-        {
-            PathFollow();
-        }
-    }
-
-    private void UpdatePath()
-    {
-        if (followEnabled && TargetInDistance() && seeker.IsDone())
-        {
+        if(seeker.IsDone())
             seeker.StartPath(rb.position, target.position, OnPathComplete);
-        }
-    }
-
-    private void PathFollow()
-    {
-        if (path == null)
-        {
-            return;
-        }
-
-        // Reached end of path
-        if (currentWaypoint >= path.vectorPath.Count)
-        {
-            return;
-        }
-
-        // See if colliding with anything
-        Vector3 startOffset = transform.position - new Vector3(0f, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
-        isGrounded = Physics2D.Raycast(startOffset, -Vector3.up, 0.05f);
-        
-        // Direction Calculation
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
-
-        // Jump
-        if (jumpEnabled && isGrounded)
-        {
-            if (direction.y > jumpNodeHeightRequirement)
-            {
-                rb.AddForce(Vector2.up * speed * jumpModifier);
-            }
-        }
-
-        // Movement
-        rb.AddForce(force);
-
-        // Next Waypoint
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-        if (distance < nextWaypointDistance)
-        {
-            currentWaypoint++;
-        }
-
-        // Direction Graphics Handling
-        if (directionLookEnabled)
-        {
-            if (rb.velocity.x > 0.05f)
-            {
-                transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else if (rb.velocity.x < -0.05f)
-            {
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-        }
     }
 
     private bool TargetInDistance()
@@ -111,12 +40,50 @@ public class EnemyAI : MonoBehaviour
         return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
     }
 
-    private void OnPathComplete(Path p)
+    void OnPathComplete(Path p)
     {
-        if (!p.error)
+        if(!p.error)
         {
             path = p;
             currentWaypoint = 0;
         }
+    }
+    
+    void FixedUpdate()
+    {
+        if(path == null)
+        return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+                return;
+        } else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if(force.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(force.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        
     }
 }
