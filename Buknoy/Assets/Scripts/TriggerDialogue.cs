2abe33d29622902f27e8a6fefdac5f75aa792b0a
Lance Martija <jacobmartija03@gmail.com>
2023-01-01 03:20:40 +0800

    fix dialogue bug where dialogue doesn't show up

--- Buknoy/Assets/Scripts/TriggerDialogue.cs
+++ Buknoy/Assets/Scripts/TriggerDialogue.cs
@@ -11,35 +11,72 @@ public class TriggerDialogue : MonoBehaviour
     private GameObject controls;
     private BoxCollider2D DialogueTriggerArea;
     
-    private void OnTriggerEnter2D(Collider2D other)
+    void Start()
     {
         pauseBtn = GameObject.Find("PauseButton").GetComponent<Button>();
         controls = GameObject.Find("Controls");
         DialogueTriggerArea = GetComponent<BoxCollider2D>();
-        
+    }
+    
+    // private void Awake() {
+    //     if (pauseBtn != null && DialogueTriggerArea != null)
+    //     {   
+    //         pauseBtn = GameObject.Find("PauseButton").AddComponent<Button>() as Button;
+    //         DialogueTriggerArea = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+    //         return;
+    //     }
+    // }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         print("Trigger Entered");
         
         if (other.tag == "Player")
         {
-            ConversationManager.OnConversationStarted += ConversationStart;
-            ConversationManager.Instance.StartConversation(myConversation);
-            ConversationManager.OnConversationEnded += ConversationEnd;
+            if (ConversationManager.Instance != null)
+            {
+                ConversationManager.Instance.StartConversation(myConversation);
+                ConversationManager.OnConversationStarted += ConversationStart;
+                ConversationManager.OnConversationEnded += ConversationEnd;
+            }
         }
     }
     
     private void ConversationStart()
     {
-        print("Trigger Destroyed");
+        print("Trigger Disabled");
         print("Buttons Disabled");
-        pauseBtn.interactable = false;
-        controls.SetActive(false);
-        Destroy(DialogueTriggerArea);
+        
+        if (pauseBtn != null)
+        {
+            pauseBtn.interactable = false;
+        }
+        
+        if (controls != null)
+        {
+            controls.SetActive(false);
+        }
+        
+        if (DialogueTriggerArea != null)
+        {
+            DialogueTriggerArea.enabled = false;
+        }
+        
     }
     
     private void ConversationEnd()
     {
         print("Buttons Enabled");
-        pauseBtn.interactable = true;
-        controls.SetActive(true);
+        
+        
+        if (pauseBtn != null)
+        {
+            pauseBtn.interactable = true;
+        }
+        
+        if (controls != null)
+        {
+            controls.SetActive(true);
+        }
     }
 }
