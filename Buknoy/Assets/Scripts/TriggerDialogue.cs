324bf8b07ba1554c3dd4fda6b1701b6207b3b25e
Lance Martija <jacobmartija03@gmail.com>
2022-12-31 15:19:03 +0800

    Update TriggerDialogue.cs

--- Buknoy/Assets/Scripts/TriggerDialogue.cs
+++ Buknoy/Assets/Scripts/TriggerDialogue.cs
@@ -6,12 +6,25 @@ using DialogueEditor;
 public class TriggerDialogue : MonoBehaviour
 {
     public NPCConversation myConversation;
+    //private AudioSource Audio;
+    private BoxCollider2D DialogueTriggerArea;
     
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Audio = ConversationManager.Instance.GetComponent<AudioSource>();
+        DialogueTriggerArea = GetComponent<BoxCollider2D>();
         print("Trigger Entered");
         
-        if (other.tag == "Player") {
+        if (other.tag == "Player")
+        {
+            ConversationManager.OnConversationStarted += ConversationStart;
             ConversationManager.Instance.StartConversation(myConversation);
         }
+    }
+    
+    private void ConversationStart()
+    {
+        print("Trigger Destroyed");
+        Destroy(DialogueTriggerArea);
     }
 }
