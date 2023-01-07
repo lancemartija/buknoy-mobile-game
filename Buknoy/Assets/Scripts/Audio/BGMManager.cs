2524372cde7eb5ee4ef7fc81887f7cc475820f12
Lance Martija <jacobmartija03@gmail.com>
2023-01-07 23:59:27 +0800

    fix whitespaces and indentation

--- Buknoy/Assets/Scripts/Audio/BGMManager.cs
+++ Buknoy/Assets/Scripts/Audio/BGMManager.cs
@@ -4,85 +4,97 @@ using UnityEngine;
 
 public class BGMManager : MonoBehaviour
 {
-
     public AudioSource BGM;
     private float fadeTime = 0f;
-
-    public AudioClip mainmenuBGM, menuhubBGM, quizmenuBGM, quizgameBGM, notesBGM, chapter0BGM, chapter1BGM, chapter2BGM, chapter3BGM;
-
-
-    
+    public AudioClip mainmenuBGM,
+        menuhubBGM,
+        quizmenuBGM,
+        quizgameBGM,
+        notesBGM,
+        chapter0BGM,
+        chapter1BGM,
+        chapter2BGM,
+        chapter3BGM;
     private static BGMManager instance = null;
     public static BGMManager Instance
-     {
-         get { return instance; }
+    {
+        get { return instance; }
     }
 
     void Start()
     {
-      BGM.clip = mainmenuBGM;
-      PlayerPrefs.SetFloat("musicVolumeSlider", 0.5f);
-      PlayerPrefs.SetFloat("soundVolumeSlider", 0.75f);
-      PlayerPrefs.SetInt("musicVolumeToggle", 1);
-      PlayerPrefs.SetInt("soundVolumeToggle", 1);
-      if (PlayerPrefs.HasKey("musicVolumeSlider"))
-      {
-        BGM.volume = PlayerPrefs.GetFloat("musicVolumeSlider");
-      }
+        BGM.clip = mainmenuBGM;
+
+        PlayerPrefs.SetFloat("musicVolumeSlider", 0.5f);
+        PlayerPrefs.SetFloat("soundVolumeSlider", 0.75f);
+        PlayerPrefs.SetInt("musicVolumeToggle", 1);
+        PlayerPrefs.SetInt("soundVolumeToggle", 1);
+
+        if (PlayerPrefs.HasKey("musicVolumeSlider"))
+        {
+            BGM.volume = PlayerPrefs.GetFloat("musicVolumeSlider");
+        }
     }
+
     private void Awake()
     {
-        if (instance != null && instance != this) {
-             Destroy(this.gameObject);
-             return;
-         } else {
-             instance = this;
-         }
-         DontDestroyOnLoad(this.gameObject);
-         BGM = GetComponent<AudioSource>();
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        BGM = GetComponent<AudioSource>();
     }
+
     //Default Triggers
     public void PlayMusic()
     {
         StartCoroutine(FadeIn(BGM, fadeTime));
     }
- 
+
     public void StopMusic()
     {
-       StartCoroutine(FadeOut(BGM, fadeTime));
+        StartCoroutine(FadeOut(BGM, fadeTime));
     }
-     public void StopMusicNow()
+
+    public void StopMusicNow()
     {
-      if (BGM.isPlaying)
+        if (BGM.isPlaying)
         {
             BGM.UnPause();
         }
-      BGM.Stop();
+        BGM.Stop();
     }
 
-    //Switch BGM 
+    //Switch BGM
     public void MainMenutoQuizMenuBGM()
     {
         BGM.clip = quizmenuBGM;
         PlayMusic();
-    }    
+    }
+
     public void MainMenutoNoteMenuBGM()
     {
         BGM.clip = notesBGM;
         PlayMusic();
     }
-    
+
     public void BacktoMenuHubBGM()
     {
-      BGM.clip = menuhubBGM;
-      PlayMusic();
+        BGM.clip = menuhubBGM;
+        PlayMusic();
     }
-    
+
     public void BacktoMainMenuBGM()
     {
         BGM.clip = mainmenuBGM;
         PlayMusic();
-    }  
+    }
 
     public void QuizMenutoQuizGameBGM()
     {
@@ -92,30 +104,30 @@ public class BGMManager : MonoBehaviour
 
     public void MainMenutoGameBGM(int chapterno)
     {
-      switch (chapterno)
-      {
-        case 0:
-          BGM.clip = chapter0BGM;
-          break;
-        case 1:
-          BGM.clip = chapter1BGM;
-          break;
-        case 2:
-          BGM.clip = chapter2BGM;
-          break;
-        case 3:
-          BGM.clip = chapter3BGM;
-          break;
-      }
-      PlayMusic();
+        switch (chapterno)
+        {
+            case 0:
+                BGM.clip = chapter0BGM;
+                break;
+            case 1:
+                BGM.clip = chapter1BGM;
+                break;
+            case 2:
+                BGM.clip = chapter2BGM;
+                break;
+            case 3:
+                BGM.clip = chapter3BGM;
+                break;
+        }
+        PlayMusic();
     }
 
-
-     //Pause BGM
+    //Pause BGM
     public void PauseBGM()
     {
         BGM.Pause();
     }
+
     public void UnPauseBGM()
     {
         BGM.UnPause();
@@ -126,25 +138,25 @@ public class BGMManager : MonoBehaviour
     }
 
     //Fade In and Fade Out BGM
-    public static IEnumerator FadeOut(AudioSource BGM, float FadeTime) 
+    public static IEnumerator FadeOut(AudioSource BGM, float FadeTime)
     {
-      float startVolume = BGM.volume;
-      while (BGM.volume > 0) 
-      {
-        BGM.volume -= startVolume * Time.deltaTime / FadeTime;
-        yield return null;
-      }
-      BGM.Stop();
-	  }
-
-    public static IEnumerator FadeIn(AudioSource BGM, float FadeTime) 
-    {
-      BGM.Play();
-      BGM.volume = 0f;
-      while (BGM.volume < PlayerPrefs.GetFloat("musicVolumeSlider")) 
-      {
-        BGM.volume += Time.deltaTime / FadeTime;
-        yield return null;
-      }
+        float startVolume = BGM.volume;
+        while (BGM.volume > 0)
+        {
+            BGM.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+        BGM.Stop();
     }
- }
+
+    public static IEnumerator FadeIn(AudioSource BGM, float FadeTime)
+    {
+        BGM.Play();
+        BGM.volume = 0f;
+        while (BGM.volume < PlayerPrefs.GetFloat("musicVolumeSlider"))
+        {
+            BGM.volume += Time.deltaTime / FadeTime;
+            yield return null;
+        }
+    }
+}
--- Buknoy/Assets/Scripts/MenuHub.cs
+++ Buknoy/Assets/Scripts/MenuHub.cs
@@ -5,44 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class MenuHub : MonoBehaviour
 {
-    void Awake() {
+    void Awake()
+    {
         GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().BacktoMenuHubBGM();
     }
-    
+
     public void GoToMenu()
     {
+        GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().StopMusic();
         SceneManager.LoadScene("StartMenu");
     }
+
     public void GoToQuiz()
     {
         GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().StopMusic();
         Invoke("LoadQuiz", 0f);
     }
+
     public void GoToLevelSelect()
     {
         SceneManager.LoadScene("LevelSelect");
     }
+
     public void GoToViewInfo()
     {
         GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().StopMusic();
         Invoke("LoadNotes", 0f);
     }
+
     public void GotoSettings()
     {
         Invoke("LoadSettings", 0f);
     }
+
     void LoadQuiz()
     {
         SceneManager.LoadSceneAsync("Quizzes");
         GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().MainMenutoQuizMenuBGM();
         Time.timeScale = 1;
-    } 
+    }
+
     void LoadNotes()
     {
         SceneManager.LoadSceneAsync("Notes");
         GameObject.FindGameObjectWithTag("BGM").GetComponent<BGMManager>().MainMenutoNoteMenuBGM();
         Time.timeScale = 1;
-    } 
+    }
+
     void LoadSettings()
     {
         SceneManager.LoadScene("Settings");
