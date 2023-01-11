7c321dbbccd685bdde448d65d44e78df10a32706
Lance Martija <jacobmartija03@gmail.com>
2023-01-11 23:06:01 +0800

    fix whitespaces and indentation

--- Buknoy/Assets/Scripts/JumpingDuck.cs
+++ Buknoy/Assets/Scripts/JumpingDuck.cs
@@ -10,6 +10,7 @@ public class JumpingDuck : MonoBehaviour
     public Transform down;
     bool land;
     private Animator anim;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,25 +20,33 @@ public class JumpingDuck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y >= up.position.y)
+        if (transform.position.y >= up.position.y)
         {
             land = true;
         }
-        if(transform.position.y <= down.position.y)
+        if (transform.position.y <= down.position.y)
         {
             land = false;
         }
-        if(land)
+        if (land)
         {
-            transform.position = Vector2.MoveTowards(transform.position,down.position,downspeed * Time.deltaTime);
-            anim.SetBool("fall",true);
-            anim.SetBool("jump",false);
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                down.position,
+                downspeed * Time.deltaTime
+            );
+            anim.SetBool("fall", true);
+            anim.SetBool("jump", false);
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position,up.position,upspeed * Time.deltaTime);
-            anim.SetBool("jump",true);
-            anim.SetBool("fall",false);
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                up.position,
+                upspeed * Time.deltaTime
+            );
+            anim.SetBool("jump", true);
+            anim.SetBool("fall", false);
         }
     }
 }
